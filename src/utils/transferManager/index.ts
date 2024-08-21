7e27ebc68200f9b23c6b2bf3d2803af9996471ba
YH <1191759350@qq.com>
2024-08-21 18:00:47 +0800

    commit

--- src/utils/transferManager/index.ts
+++ src/utils/transferManager/index.ts
@@ -1,32 +1,56 @@
-import { ConcurrentTask } from './utils/concurrentTask';
+import { ConcurrentTask } from './utils/ConcurrentLimit';
 import request from '../request';
 import { getFileHash } from '../utils';
-import type { Task } from './utils/concurrentTask/task';
+import type { Task } from './utils/ConcurrentLimit/task';
 import axios, {
   type AxiosInstance,
   type AxiosProgressEvent,
   type AxiosRequestConfig,
   type CancelTokenSource
 } from 'axios';
+import { EventHelper } from './utils/eventHelper';
 
-type Options = {
-  uploadPath: string;
-  uploadName: string;
+type UploadTaskOptions = {
+  path: string;
+  name: string;
   file: File;
-  onProgress?: (progress: number) => void;
-  onChange?: (status: string) => void;
 };
 
-const concurrentTask = new ConcurrentTask({ limit: 6 });
+type UploadTaskStatus =
+  | 'init'
+  | 'error'
+  | 'ready'
+  | 'play'
+  | 'pause'
+  | 'success'
+  | 'fail'
+  | 'cancel';
+
+type UploadTaskStep = 'checking' | 'uploading' | 'creating';
+
+type UploadTaskSimpleUploadParams = {
+  uploadUrl: string;
+};
+
+type UploadTaskMultipartUploadParams = {
+  uploadId: string;
+  number: number;
+  size: number;
+  total: number;
+  partSize: number;
+  list: { url: string; partNumber: number; etag: string }[];
+};
 
 class MultipartRequest {
   #awaitQueue: { task: Task; cancel: CancelTokenSource['cancel'] }[] = []; // 等待队列
-  #concurrentTask: ConcurrentTask; // 并发对象实例
+  #concurrentTask: ConcurrentTask = new ConcurrentTask({ limit: 6 }); // 并发对象实例
 
-  constructor(concurrentTask: ConcurrentTask) {
-    this.#concurrentTask = concurrentTask;
-  }
-
+  /**
+   * @description: 添加请求
+   * @param {AxiosInstance} request 请求实例
+   * @param {AxiosRequestConfig} config 请求配置
+   * @return {Task} 任务实例
+   */
   add(request: AxiosInstance, config: AxiosRequestConfig): Task {
     const { token, cancel } = axios.CancelToken.source();
     const task = this.#concurrentTask.push(request, { ...config, cancelToken: token });
@@ -34,87 +58,60 @@ class MultipartRequest {
     return task;
   }
 
+  /**
+   * @description: 清除请求
+   */
   clear() {
-    this.#awaitQueue.forEach((config) => {
-      this.#concurrentTask.deleteTask(config.task);
-      config.cancel();
-    });
+    this.#concurrentTask.clear();
+    this.#awaitQueue.forEach((config) => config.cancel());
     this.#awaitQueue = [];
   }
 }
 
-class UploadTask {
-  #status: 'pending' | 'upload' | 'pause' | 'success' | 'fail' | 'cancel' = 'pending';
-  #progress: number = 0;
-  #options: Options;
-  #result: any;
-  #uploadUrl: string | undefined;
-  #multipartRequest: MultipartRequest;
+class UploadTask extends EventHelper {
+  #options: UploadTaskOptions; // 配置项
+  #status: UploadTaskStatus = 'init'; // 状态: init: 初始化, error: 异常, ready: 就绪, play: 运行, pause: 暂停, success: 成功, fail: 失败, cancel: 取消
+  #step: UploadTaskStep = 'checking'; // 步骤: checking: 校验文件, uploading: 上传文件, creating: 创建文件
+  #progress: number = 0; // 上传进度
+  #hash: string = ''; // 文件hash
+  #multipartRequest: MultipartRequest = new MultipartRequest(); // 并发请求管理器
+  #uploadType: 'simple' | 'multipart'; // 上传类型
+  #simpleUploadParams?: UploadTaskSimpleUploadParams; // 简单上传参数
+  #multipartUploadParams?: UploadTaskMultipartUploadParams; // 分片上传参数
 
+  /**
+   * @description: 状态
+   */
   get status() {
     return this.#status;
   }
 
+  /**
+   * @description: 上传进度
+   */
   get progress() {
     return this.#progress;
   }
 
-  get result() {
-    return this.#result;
-  }
-
-  constructor(options: Options) {
+  constructor(options: UploadTaskOptions) {
+    super();
     this.#options = options;
-    this.#multipartRequest = new MultipartRequest(concurrentTask);
+    this.#uploadType = options.file.size > 1024 * 1024 * 100 ? 'multipart' : 'simple';
+    this.init();
   }
 
-  async #checkHash() {
+  /**
+   * @description: 初始化
+   */
+  async init() {
+    this.#progress = 0;
+    this.#changeStatus('init');
     try {
-      const res = await this.#multipartRequest.add(request, {
-        url: '/fileSystem/createFile',
-        method: 'post',
-        data: {
-          folderPath: this.#options.uploadPath,
-          fileName: this.#options.uploadName,
-          fileHash: await getFileHash(this.#options.file),
-          fileSize: this.#options.file.size
-        }
-      });
-
-      if (res.data.data.uploadUrl) {
-        this.#uploadUrl = res.data.data.uploadUrl;
-        this.#uploadFile();
-      } else {
-        this.#status = 'success';
-      }
+      this.#hash = this.#hash || (await getFileHash(this.#options.file)); // 计算hash值
+      this.#step = 'checking';
+      this.#changeStatus('ready');
     } catch (err) {
-      if (axios.isCancel(err)) {
-        this.#status = 'cancel';
-      } else {
-        this.#status = 'fail';
-      }
-    }
-  }
-
-  async #uploadFile() {
-    try {
-      await this.#multipartRequest.add(request, {
-        url: this.#uploadUrl,
-        method: 'put',
-        data: this.#options.file,
-        onUploadProgress: (progressEvent: AxiosProgressEvent) => {
-          if (progressEvent.total) {
-            this.#progress = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-          }
-        }
-      });
-      this.#status = 'success';
-    } catch (err) {
-      if (axios.isCancel(err)) {
-        this.#status = 'cancel';
-      } else {
-        this.#status = 'fail';
-      }
+      this.#changeStatus('error');
     }
   }
 
@@ -122,11 +119,29 @@ class UploadTask {
    * @description: 开始
    */
   async start() {
-    this.#status = 'upload';
-    if (!this.#uploadUrl) {
-      this.#checkHash();
-    } else {
-      this.#uploadFile();
+    if (this.#status !== 'ready' && this.#status !== 'pause' && this.#status !== 'fail') return;
+    let isExist: boolean;
+    this.#changeStatus('play');
+    try {
+      // 校验文件
+      if (this.#step === 'checking') {
+        isExist = await this.#checkFile();
+        this.#step = isExist ? 'creating' : 'uploading';
+      }
+
+      // 上传文件
+      if (this.#step === 'uploading') {
+        await this.#uploadFile();
+        this.#step = 'creating';
+      }
+
+      // 创建文件
+      if (this.#step === 'creating') {
+        await this.#createFile();
+        this.#changeStatus('success');
+      }
+    } catch (err) {
+      if (err !== 'cancel') this.#changeStatus('fail');
     }
   }
 
@@ -134,23 +149,155 @@ class UploadTask {
    * @description: 暂停
    */
   pause() {
-    this.#status = 'pause';
-    this.#multipartRequest.clear();
+    if (this.#status !== 'play') return;
+    this.#changeStatus('pause');
   }
 
   /**
    * @description: 取消
    */
   cancel() {
-    this.#status = 'cancel';
-    this.#multipartRequest.clear();
+    if (this.#status == 'success' || this.#status === 'fail' || this.#status === 'cancel') return;
+    this.#changeStatus('cancel');
+  }
+
+  /**
+   * @description: 校验文件
+   */
+  async #checkFile(): Promise<boolean> {
+    if (this.#status !== 'play') return Promise.reject('cancel');
+    try {
+      const res = await this.#multipartRequest.add(request, {
+        url: '/fileSystem/checkHash',
+        method: 'post',
+        data: {
+          folderPath: this.#options.path,
+          fileName: this.#options.name,
+          fileHash: this.#hash,
+          fileSize: this.#options.file.size
+        }
+      });
+      const data = res.data.data;
+
+      if (data.isExist) return true;
+
+      if (this.#uploadType === 'simple') {
+        this.#simpleUploadParams = { uploadUrl: data.uploadUrl };
+      } else {
+        this.#multipartUploadParams = {
+          uploadId: data.uploadId,
+          list: [],
+          number: 1,
+          size: 20,
+          total: Math.ceil(this.#options.file.size / (1024 * 1024)),
+          partSize: 1024 * 1024
+        };
+      }
+      return false;
+    } catch (err) {
+      return Promise.reject(axios.isCancel(err) ? 'cancel' : err);
+    }
+  }
+
+  /**
+   * @description: 上传文件
+   */
+  async #uploadFile() {
+    if (this.#status !== 'play') return Promise.reject('cancel');
+    try {
+      if (this.#uploadType === 'simple') {
+        await this.#multipartRequest.add(axios, {
+          url: this.#simpleUploadParams?.uploadUrl,
+          method: 'put',
+          data: this.#options.file,
+          onUploadProgress: (progressEvent: AxiosProgressEvent) => {
+            if (progressEvent.total) {
+              this.#progress = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+            }
+          }
+        });
+      } else {
+        const handleMultipartUpload = async () => {
+          if (this.#multipartUploadParams!.number >= this.#multipartUploadParams!.total) return;
+          const res = await this.#multipartRequest.add(request, {
+            url: '/fileSystem/multipartUpload',
+            method: 'post',
+            data: {
+              uploadId: this.#multipartUploadParams?.uploadId,
+              number: this.#multipartUploadParams?.number,
+              size: this.#multipartUploadParams?.size
+            }
+          });
+          this.#multipartUploadParams?.list.push(
+            ...res.data.data.list.map((url: string) => ({
+              url,
+              partNumber: this.#multipartUploadParams!.number++,
+              etag: ''
+            }))
+          );
+
+          for (const item of this.#multipartUploadParams?.list || []) {
+            this.#multipartRequest
+              .add(axios, {
+                url: item.url,
+                method: 'put',
+                data: this.#options.file.slice(
+                  (item.partNumber - 1) * 1024 * 1024,
+                  item.partNumber * 1024 * 1024
+                )
+              })
+              .then((res) => {
+                item.etag = res.headers['etag'];
+                this.#progress =
+                  this.#options.file.size /
+                  (this.#multipartUploadParams!.list.reduce(
+                    (count, item) => (item.etag ? ++count : count),
+                    0
+                  ) *
+                    this.#multipartUploadParams!.partSize);
+              });
+          }
+
+          handleMultipartUpload();
+        };
+
+        handleMultipartUpload();
+      }
+    } catch (err) {
+      return Promise.reject(axios.isCancel(err) ? 'cancel' : err);
+    }
+  }
+
+  /**
+   * @description: 创建文件
+   */
+  async #createFile() {}
+
+  /**
+   * @description: 改变状态
+   * @param {UploadTaskStatus} status 状态
+   */
+  #changeStatus(status: UploadTaskStatus) {
+    if (this.#status === status) return;
+    this.#status = status;
+    this.emit('status', status);
+  }
+
+  /**
+   * @description: 改变暂停状态
+   * @param {boolean} paused 是否暂停
+   */
+  #changePaused(paused: boolean) {
+    if (this.#paused === paused) return;
+    this.#paused = paused;
+    this.emit('paused', paused);
   }
 }
 
 function main() {
   const uploadTask = new UploadTask({
-    uploadPath: '/utils/test',
-    uploadName: 'test.jpg',
+    path: '/utils/test',
+    name: 'test.jpg',
     file: new File([], ''),
     onProgress: (progress) => {
       console.log(progress);
--- src/utils/transferManager/utils/eventHelper/EventHelper.ts
+++ src/utils/transferManager/utils/eventHelper/EventHelper.ts
@@ -1,20 +1,42 @@
-export const EVENT_MAP = Symbol('EVENT_MAP');
+/*
+ * @FileName: 事件助手类
+ * @FilePath: \cloud-disk\src\utils\transferManager\utils\eventHelper\eventHelper.ts
+ * @Author: YH
+ * @Date: 2024-08-13 16:39:35
+ * @LastEditors: YH
+ * @LastEditTime: 2024-08-13 16:42:01
+ * @Description:
+ */
+export const EVENT_MAP = Symbol('EVENT_MAP'); // 事件映射属性名
 
 export class EventHelper {
   [EVENT_MAP]: Record<string, { callback: Function; once: boolean }[]> = {};
 
-  constructor() {}
-
+  /**
+   * @description: 监听事件
+   * @param {string} eventName 事件名称
+   * @param {Function} callback 回调函数
+   */
   on(eventName: string, callback: Function) {
     const configs = this[EVENT_MAP][eventName] || [];
     this[EVENT_MAP][eventName] = configs.concat([{ callback, once: false }]);
   }
 
+  /**
+   * @description: 仅监听一次事件
+   * @param {string} eventName 事件名称
+   * @param {Function} callback 回调函数
+   */
   once(eventName: string, callback: Function) {
     const configs = this[EVENT_MAP][eventName] || [];
     this[EVENT_MAP][eventName] = configs.concat([{ callback, once: true }]);
   }
 
+  /**
+   * @description: 发送事件
+   * @param {string} eventName 事件名称
+   * @param {any[]} args 参数
+   */
   emit(eventName: string, ...args: any[]) {
     const configs = this[EVENT_MAP][eventName] || [];
     const newConfigs = configs.filter((config) => {
@@ -24,6 +46,11 @@ export class EventHelper {
     this[EVENT_MAP][eventName] = newConfigs;
   }
 
+  /**
+   * @description: 关闭事件
+   * @param {string} eventName 事件名称
+   * @param {Function} callback 回调函数
+   */
   off(eventName: string, callback: Function) {
     const configs = this[EVENT_MAP][eventName] || [];
     const pos = configs.findIndex((config) => config.callback === callback);
