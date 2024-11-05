297847f781ea9ea36c50073c1405c8d65f7d3214
YH <1191759350@qq.com>
2024-11-05 17:59:50 +0800

    commit

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,8 +1,11 @@
 import { contextBridge } from 'electron';
+import * as baiduyun from '@/api/baiduyun';
+import type { ElectronApi } from 'common/types/electron-api';
 
-export const electronApi = {
+export const electronApi: ElectronApi = {
   desktop: true, // 是否桌面端
-  version: process.versions.electron // electron版本
+  version: process.versions.electron, // electron版本
+  baiduyun
 };
 
 contextBridge.exposeInMainWorld('electronApi', electronApi);
