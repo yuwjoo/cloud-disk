113ed49758764d8465fe53c8a67ccb60cce3ec46
YH <1191759350@qq.com>
2024-11-07 13:20:40 +0800

    优化electronapi类型声明

--- src/common/types/electron-api.ts
+++ src/common/types/electron-api.ts
@@ -1,19 +1,5 @@
 import { IpcRendererEvent } from 'electron';
-import {
-  ApiCreateDirRequest,
-  ApiCreateDirResponse,
-  ApiDeleteFileRequest,
-  ApiDeleteFileResponse,
-  ApiGetDownloadUrlRequest,
-  ApiGetDownloadUrlResponse,
-  ApiGetListRequest,
-  ApiGetListResponse,
-  ApiRenameFileRequest,
-  ApiRenameFileResponse,
-  ApiSearchFileRequest,
-  ApiSearchFileResponse
-} from './api/baiduyun';
-import { IpcEventMap } from './ipc';
+import { IpcChannelMap, IpcEventMap } from './ipc';
 
 /**
  * @description: electron api
@@ -63,25 +49,29 @@ export interface RemoveAllListener {
   <K extends keyof IpcEventMap>(name: K): void;
 }
 
+export type IpcRequestFun<K extends keyof IpcChannelMap> = (
+  ...args: IpcChannelMap[K]['params']
+) => IpcChannelMap[K]['result'];
+
 /**
  * @description: 窗口api
  */
 export interface WindowApi {
-  maximize(): void; // 最大化
-  minimize(): void; // 最小化
-  restore(): void; // 还原
-  close(): void; // 关闭
-  toggleFullScreen(isFull?: boolean): void; // 切换全屏状态
+  maximize: IpcRequestFun<'window-maximize'>; // 最大化
+  minimize: IpcRequestFun<'window-minimize'>; // 最小化
+  restore: IpcRequestFun<'window-restore'>; // 还原
+  close: IpcRequestFun<'window-close'>; // 关闭
+  toggleFullScreen: IpcRequestFun<'window-toggle-full-screen'>; // 切换全屏状态
 }
 
 /**
  * @description: 百度云api
  */
 export interface BaiduyunApi {
-  searchFile(data: ApiSearchFileRequest): Promise<ApiSearchFileResponse>; // 模糊查询文件
-  getList(data: ApiGetListRequest): Promise<ApiGetListResponse>; // 获取列表数据
-  createDir(data: ApiCreateDirRequest): Promise<ApiCreateDirResponse>; // 创建文件夹
-  renameFile(data: ApiRenameFileRequest): Promise<ApiRenameFileResponse>; // 重命名文件
-  deleteFile(data: ApiDeleteFileRequest): Promise<ApiDeleteFileResponse>; // 删除文件
-  getDownloadUrl(data: ApiGetDownloadUrlRequest): Promise<ApiGetDownloadUrlResponse>; // 获取下载地址
+  searchFile: IpcRequestFun<'baiduyun-search-file'>; // 模糊查询文件
+  getList: IpcRequestFun<'baiduyun-get-list'>; // 获取列表数据
+  createDir: IpcRequestFun<'baiduyun-create-dir'>; // 创建文件夹
+  renameFile: IpcRequestFun<'baiduyun-rename-file'>; // 重命名文件
+  deleteFile: IpcRequestFun<'baiduyun-delete-file'>; // 删除文件
+  getDownloadUrl: IpcRequestFun<'baiduyun-get-download-url'>; // 获取下载地址
 }
