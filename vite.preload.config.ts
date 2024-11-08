b5991a65465f62f4282d8e5e54b8f4f399f05089
YH <1191759350@qq.com>
2024-11-08 16:32:30 +0800

    commit

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  publicDir: false,
   resolve: {
     alias: {
       common: fileURLToPath(new URL('./src/common', import.meta.url)),
