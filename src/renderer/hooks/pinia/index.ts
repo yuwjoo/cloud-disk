faa4a1f301eda8aa57d78edc58e1084a30c99d06
YH <1191759350@qq.com>
2024-11-21 17:39:55 +0800

    commit

--- src/renderer/hooks/pinia/index.ts
+++ src/renderer/hooks/pinia/index.ts
@@ -1,12 +1,11 @@
 import { createPinia, type Pinia } from 'pinia';
 
-let pinia: Pinia | null = null;
+const pinia = createPinia();
 
 /**
  * @description: 使用pinia实例
  * @return {Pinia} pinia
  */
 export function usePinia(): Pinia {
-  if (!pinia) pinia = createPinia();
   return pinia;
 }
--- src/renderer/hooks/vueRouter/index.ts
+++ src/renderer/hooks/vueRouter/index.ts
@@ -1,10 +1,13 @@
 import routes from '@/router';
-import { createRouter as createVueRouter, createWebHashHistory, type Router } from 'vue-router';
+import {
+  createRouter,
+  createWebHashHistory,
+  type RouteLocationNormalizedLoaded,
+  type Router
+} from 'vue-router';
 import { beforeEach } from './interceptor';
 
-const router = createVueRouter({ history: createWebHashHistory(import.meta.env.BASE_URL), routes });
-
-router.beforeEach(beforeEach);
+const router = createRouter({ history: createWebHashHistory(import.meta.env.BASE_URL), routes });
 
 /**
  * @description: 使用router实例
@@ -13,3 +16,17 @@ router.beforeEach(beforeEach);
 export function useRouter(): Router {
   return router;
 }
+
+/**
+ * @description: 使用route实例
+ * @return {RouteLocationNormalizedLoaded} route
+ */
+export function useRoute(): RouteLocationNormalizedLoaded {
+  return new Proxy(router.currentRoute, {
+    get(target: Ref<RouteLocationNormalizedLoaded>, key: keyof RouteLocationNormalizedLoaded) {
+      return target.value[key];
+    }
+  }) as unknown as RouteLocationNormalizedLoaded;
+}
+
+router.beforeEach(beforeEach);
